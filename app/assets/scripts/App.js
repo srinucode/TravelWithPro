30a7ba05c751135978149329b1b617fb3fd02333
Pintu <ryongpintu@gmail.com>
2018-08-08 04:01:11 +0530

    Completed the modal section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,15 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import revealOnScroll from './modules/revealOnScroll';
 import stickyHeader from './modules/stickyHeader';
+import modal from './modules/modal';
 
 var mobileMenu = new MobileMenu();
 new revealOnScroll($(".feature-items"),"85%");
 new revealOnScroll($(".testimonial"),"60%");
 
 var stickyHeaderObj = new stickyHeader();
+var modalObj = new modal();
+
 
 
 
