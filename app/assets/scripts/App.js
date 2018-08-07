600e4f1cf92575573c61b327e3901808e5fd2cbf
Pintu <ryongpintu@gmail.com>
2018-08-07 15:13:19 +0530

    Sticky header has completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import revealOnScroll from './modules/revealOnScroll';
+import stickyHeader from './modules/stickyHeader';
 
 var mobileMenu = new MobileMenu();
 new revealOnScroll($(".feature-items"),"85%");
 new revealOnScroll($(".testimonial"),"60%");
+var stickyHeaderObj = new stickyHeader();
